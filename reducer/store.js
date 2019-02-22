f7b32ff57af70e9f162b583b88ec2e150dd47b1f
tanakorn0314 <tanakorn0412@gmail.com>
2019-02-22 23:01:38 +0700

    store

--- reducer/store.js
+++ reducer/store.js
@@ -1,16 +1,16 @@
-import { CREATE_USER, STORE_TOKEN } from '../action/action-types';
+import actionTypes from '../action/action-types';
 import { createStore } from 'redux';
 
 const initialState = {};
 
 function reducer(state = initialState, action) {
     switch (action.type) {
-        case CREATE_USER:
+        case actionTypes.CREATE_USER:
             return {
                 ...state,
                 user: action.payload
             };
-        case STORE_TOKEN:
+        case actionTypes.STORE_TOKEN:
             return {
                 ...state,
                 token: action.payload
