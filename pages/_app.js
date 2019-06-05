f8e0f17b70dcbedaa2df049cef00147ea0a1b2cf
tanakorn0314 <tanakorn0412@gmail.com>
2019-06-05 23:03:27 +0700

    create server

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import withRedux from 'next-redux-wrapper';
+import initialStore from '../redux/store';
+import { Provider } from 'react-redux';
+
+class CustomApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+        const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+        return { pageProps };
+    }
+
+    render() {
+        const { Component, pageProps, store } = this.props;
+        return (
+            <Container>
+                <Provider store={store}>
+                    <Component pageContext={this.pageContext} {...pageProps} />
+                </Provider>
+            </Container>
+        )
+    }
+}
+
+export default withRedux(initialStore)(CustomApp);
