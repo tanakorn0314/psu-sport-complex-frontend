f556f81178fadaa0858c8de7db5917c3ff9337e4
tanakorn0314 <tanakorn0412@gmail.com>
2019-07-08 15:33:14 +0700

    proper preview

--- components/postBox/index.js
+++ components/postBox/index.js
@@ -3,20 +3,27 @@ import StyledWrapper from './style';
 import { Link } from '../../i18n';
 import { Typography } from 'antd';
 import { H3 } from '../typo';
+import cheerio from 'cheerio';
+import { withNamespaces } from '../../i18n';
 
 const { Paragraph } = Typography;
 
 class PostBox extends React.Component {
     render() {
-        const { newsId, img, title, content } = this.props;
+        const { t, newsId, img, title, content } = this.props;
+        const $ = cheerio('p', content);
+        const text = $.text();
+
+        let contentText = text.length > 0 ? text : t('read more');
+
         return (
             <StyledWrapper>
                 <Link href={`/news?newsId=${newsId}`}>
                     <a className='link'>
-                        <img src={`/news/image/${img}`} width='100%' style={{marginBottom: 5}}/>
+                        <img src={`/news/image/${img}`} width='100%' height='180vw' style={{marginBottom: 5}}/>
                         <H3>{title}</H3>
                         <Paragraph ellipsis={{ rows: 3 }} style={{ margin: 0 }}>
-                            <div className='content' dangerouslySetInnerHTML={{ __html: content }} />
+                            <div className='content'>{contentText}</div>
                         </Paragraph>
                     </a>
                 </Link>
@@ -25,4 +32,4 @@ class PostBox extends React.Component {
     }
 }
 
-export default PostBox;
+export default withNamespaces('common')(PostBox);
